b96d96686169eb9e965ad696f89baa8c9790a280
Vera Ho <vh71886@gmail.com>
2022-05-06 15:19:49 -0400

    finished #show

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -5,7 +5,8 @@ class CatsController < ApplicationController
     end
 
     def show
-
+        @cat = Cat.find(params[:id])
+        render :show
     end
 
     def edit
@@ -19,7 +20,7 @@ class CatsController < ApplicationController
 
 
     def new
-
+        
     end
 
     def create
