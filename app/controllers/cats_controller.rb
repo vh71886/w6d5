23933f785ca98bef4aa4c0e5230b9fe92fb6fe23
nikumar1206 <nikumar1206@gmail.com>
2022-05-06 16:21:55 -0400

    prefilled filled

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -15,9 +15,14 @@ class CatsController < ApplicationController
     end
 
 
-    def update 
-
-    end
+    def update
+        @cat = Cat.find(params[:id])
+        if @cat.update_attributes(cat_params)
+          redirect_to cat_url(@cat)
+        else
+          render :edit
+        end
+      end
 
 
     def new
@@ -26,12 +31,28 @@ class CatsController < ApplicationController
     end
 
     def create
-
-    end
+        @cat = Cat.new(cat_params)
+        if @cat.save
+          redirect_to cat_url(@cat)
+        else
+          render :new
+        end
+      end
 
 
     def destroy
+        @cat = Cat.find(params[:id])
+        if @cat.destroy
+            redirect cats_url
+        else
+            render json: @cat.errors.full_messages, status: :unprocessable_entity
+        end
+
 
     end
 
+    private
+    def cat_params
+        params.require(:cat).permit(:sex, :color, :birth_date, :description, :name)
+    end
 end
